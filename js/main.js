b52fd092597505e392cf0b3b194b2e736c0f0d97
Kelly Carmichael <digikidd@gmail.com>
2016-05-29 10:02:09 -0500

    no message

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+// Function to get input value.
+// $('#add-btn').click(function() {
+// var textarea_value = $("#list-item").val();
+// if(textarea_value=='') {
+// alert("Enter something Jon likes!!");
+// }else{
+// 	// alert(textarea_value);
+// 	$('ul').append('<li>'+textarea_value+'</li>');
+// 	$('#list-item').val("");
+// }
+// });
+
+	
+});
