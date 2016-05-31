94cb2dc896cce1254eefe37257a743224837dcf2
Kelly Carmichael <digikidd@gmail.com>
2016-05-30 21:24:35 -0500

    code changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var listTag1 = null;
-    var listTag2 = "<div id='list-id' class='col-md-1 card blue darken-1 z-depth-2'><div class='card-content white-text'><span class='card-title'>";
+    var listTag2 = "<div id='list-id' class='col-md-1 card blue darken-4 z-depth-2'><div class='card-content white-text'><span class='card-title'>";
     var listTag3 = "</span></div></div></li>";
     var divTag = null;
 
@@ -10,37 +10,48 @@ $(document).ready(function() {
     });
 
     function addItem(e) {
-        if (e.which === 13 || e.type == "click") {
-            var itemToAdd = $('#txt-input').val();
-            listTag1 = listTag2 + itemToAdd + listTag3;
-            divTag = $('<li />', { html: listTag1 }).appendTo('ul.shop-list');
-            $('.btn-danger').show();
-            $('#txt-input').val("");
+        var txtList = $("#txt-input").val();
+        if (txtList != '') {
+            var exist = $('#shop-ul > li').text().toLowerCase().indexOf(txtList.toLowerCase());
+            if (exist == -1) {
+                var itemToAdd = $('#txt-input').val();
+                listTag1 = listTag2 + itemToAdd + listTag3;
+                divTag = $('<li />', { html: listTag1 }).appendTo('ul.shop-list');
+                $('.btn-danger').show();
+                $('#txt-input').val("");
+                $('#empty').fadeOut('slow');
+            }
+            else {
+                alert('Duplicate item');
+            }
         } else {
-            // alert('Please enter an item!');
+            alert('Please enter an item!');
         }
     }
 
-    // $('ul').on('click','button' , function(el){
-    //     $(this).parent().remove()
-    // });
-    // });
-
     $('#add-btn').click(addItem);
-    $('#txt-input').keydown(addItem);
 
+    $(document).on("keypress", function(e) {
+        if (e.which == 13) {
+            addItem();
+        }
+    });
 
-    $('li a.clearitem').live('click', function () {
-  $(this).parent().remove();
-});
-    // This is supposed to clear an item when it is clicked.
-    // $(this).click(function() {
-    //     $('#shop-ul li:last').fadeOut("slow");
-    // });
+    $(function() {
+        $('#shop-ul').on("click", "li", function() {
+            $(this).fadeOut("slow");
+            $(this).remove();
+            if ($('#shop-ul > li').length == 0) {
+                $('#empty').fadeIn('slow');
+            }
+        })
+    });
+
     //This is supposed to clear entire list.
     $('#clear-btn').click(function() {
         $("div").remove("#list-id");
         $('.btn-danger').hide();
+        $('#empty').fadeIn('slow');
     });
 
 });
