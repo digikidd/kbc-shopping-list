bd0901aa0a7f4e6f7655a941867576d1e7a60a29
Kelly Carmichael <digikidd@gmail.com>
2016-06-02 14:52:33 -0500

    Final changes.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     //Creating and preloading some variables to use during function calls.
     var listTag1 = null;
-    var listTag2 = "<div id='list-id' class='col-md-1 card-small blue darken-4 z-depth-2'><div class='card-content white-text'><span class='card-title'>";
-    var listTag3 = "</span></div></div></li>";
+    var listPreTag = "<li class='ui-state-default'>";
+    var listPostTag = "</li>";
     var divTag = null;
 
     //This function was created to test if data is present in user input and append to a UL. This also checks for duplicate items and alerts user.
@@ -12,15 +12,16 @@ $(document).ready(function() {
             var exist = $('#sortable > li').text().toLowerCase().indexOf(txtList.toLowerCase());
             if (exist == -1) {
                 var itemToAdd = $('#txt-input').val();
-                listTag1 = listTag2 + itemToAdd + listTag3;
-                divTag = $("<li class='ui-state-default'/>", { html: listTag1 }).fadeIn('slow').appendTo('ul.shop-list');
+                listTag1 = listPreTag + itemToAdd + listPostTag;
+                console.log(listTag1)
+                $(listTag1).fadeIn('slow').appendTo('#sortable');
                 $('#txt-input').val("");
                 $('#empty').fadeOut('slow');
             } else {
-                alert('Duplicate item');
+                swal({title: 'Oops: Duplicate Item.',width: 300,padding: 10,background: '#D5DBF1'})
             }
         } else {
-            alert('Please enter an item!');
+            swal({title: 'Please enter an item.',width: 300,padding: 10,background: '#D5DBF1'})
         }
     }
     //This calls a created function to add user input to shopping list
@@ -33,11 +34,11 @@ $(document).ready(function() {
     });
     //Handling deleting LIs individually and displaying empty list icon when none are present.
     $(function() {
-        $('#shop-ul').on("click", "li", function() {
+        $('#sortable').on("click", "li", function() {
             //callback function needed here to show full effect of fade before being removed.
             $(this).fadeOut(600, function() {
                 $(this).remove();
-                if ($('#shop-ul > li').length == 0) {
+                if ($('#sortable > li').length == 0) {
                     $('#empty').fadeIn('slow');
                 }
             })
@@ -45,8 +46,8 @@ $(document).ready(function() {
     });
     //This function clears every item from the list.
     $('#clear-btn').click(function() {
-        $("#list-id").fadeOut('slow');
-        $("div").remove("#list-id");
+        $(".ui-state-default").fadeOut('slow');
+        $("li").remove(".ui-state-default");
         $('#empty').fadeIn('slow');
     });
 
